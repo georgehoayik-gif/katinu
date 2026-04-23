816b10eb3ce7a231f6d06fe9c68bc762e66ef8eb
georgehoayik-gif <georgehoayik@gmail.com>
2026-04-23 16:46:42 -0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y wget gnupg
+
+# instala Chrome
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list
+
+apt-get update
+apt-get install -y google-chrome-stable
